43b9b060318e8db1591fb1ff8ef70d851eacf046
Johan Larsson <johan@johanl.se>
2016-12-03 09:05:42 +0100

    Boyscouting

--- utils/file.js
+++ utils/file.js
@@ -1,6 +1,6 @@
-import fs from 'fs';
-import Task from 'data.task';
-import { compose } from 'ramda';
+import fs from "fs";
+import Task from "data.task";
+import { compose } from "ramda";
 
 // read :: String -> Task Error Buffer
 const read = (path) => new Task((reject, resolve) => {
@@ -11,7 +11,7 @@ const read = (path) => new Task((reject, resolve) => {
   });
 
 // decode :: Task Error Buffer -> Task Error String
-const decode = (buffer) => buffer.map(a => a.toString('utf-8'));
+const decode = (buffer) => buffer.map(a => a.toString("utf-8"));
 
 // readFileAsString :: String -> Task Error String
 const readAsString = compose(decode, read);
