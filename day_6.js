59e9567528d940ca63fdce780fb1a56478d27dc6
Johan Larsson <johan.larsson@funkismultimedia.se>
2016-12-06 12:09:29 +0100

    Add solution for part 2

--- day_6.js
+++ day_6.js
@@ -50,15 +50,31 @@ const FilePath = String;
 // getKeyWithHighestValue :: {*} -> String
 const getKeyWithHighestValue = compose(prop(0), last, sortBy(prop(1)), toPairs);
 
+// getKeyWithLowestValue :: {*} -> String
+const getKeyWithLowestValue = compose(prop(0), head, sortBy(prop(1)), toPairs);
+
 // getMostFrequentLetter :: [Char] -> Char
 const getMostFrequentLetter = compose(getKeyWithHighestValue, countBy(toLower));
+
+// getLeastFrequentLetter :: [Char] -> Char
+const getLeastFrequentLetter = compose(getKeyWithLowestValue, countBy(toLower));
 
 // unscrambleMessage :: String -> String
 const unscrambleMessage = compose(join(""), map(getMostFrequentLetter), transpose, map(split("")), split("\n"));
 
+// unscrambleOriginalMessage :: String -> String
+const unscrambleOriginalMessage = compose(join(""), map(getLeastFrequentLetter), transpose, map(split("")), split("\n"));
+
 // unscrambleMessageFromFile :: FilePath -> Task Error String
 const unscrambleMessageFromFile = compose(map(unscrambleMessage), readAsString);
 
+// unscrambleOriginalMessageFromFile :: FilePath -> Task Error String
+const unscrambleOriginalMessageFromFile = compose(map(unscrambleOriginalMessage), readAsString);
+
 unscrambleMessageFromFile("input/day_6.txt").fork(console.error, (message) => {
   console.log(`The message is: '${message}'`);
+});
+
+unscrambleOriginalMessageFromFile("input/day_6.txt").fork(console.error, (message) => {
+  console.log(`The original message is: '${message}'`);
 });
